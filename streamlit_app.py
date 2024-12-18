48fffe8a2d11efa8570ea343beb88dbfb14eae32
LEANDRO CRUZ <85414141+leandrocruz17@users.noreply.github.com>
2024-12-18 11:50:59 +0000

    camera

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,3 +4,7 @@ st.title("🎈 My new app")
 st.write(
     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
 )
+enable = st.checkbox("Enable camera")
+picture = st.camera_input("Take a picture", disabled=not enable)
+if picture:
+    st.image(picture)
