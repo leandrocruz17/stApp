96c67fe8963799f58a553ec665d85becb1f1323c
LEANDRO CRUZ <85414141+leandrocruz17@users.noreply.github.com>
2024-12-18 12:20:37 +0000

    camera

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,4 +8,10 @@ enable = st.checkbox("Habilitar Camera")
 picture = st.camera_input("Registre uma foto", disabled=not enable)
 if picture:
     #st.image(picture)
-    st.write(f"Registro efetuado em {now()}!")
+    from datetime import datetime 
+    # Captura a data e hora atual 
+    now = datetime.now() 
+    # Formata a data e hora 
+    formatted_now = now.strftime("%d/%m/%Y %H:%M:%S") 
+    # # Exibe a data e hora no Streamlit 
+    st.write(f"Registro efetuado em {formatted_now}!")
